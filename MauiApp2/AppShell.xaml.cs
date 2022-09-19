891ad0279ae32e0e56b82eb1b3516de54f9b0987
Senpai <65557316+BeautifulSenpai@users.noreply.github.com>
2022-09-19 16:21:39 +0300

    Update AppShell.xaml.cs

--- MauiApp2/AppShell.xaml.cs
+++ MauiApp2/AppShell.xaml.cs
@@ -6,6 +6,6 @@ public partial class AppShell : Shell
 	{
 		InitializeComponent();
 		
-		Routing.RegisterRoute(nameof(DetailPage), typeof(DetailPage));2
+		Routing.RegisterRoute(nameof(DetailPage), typeof(DetailPage));
 	}
 }
